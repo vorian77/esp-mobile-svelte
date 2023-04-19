61b4a13735712b5d9b2b03b29802413d2af319e4
Phyllip Hall <phall776@gmail.com>
2023-04-19 19:03:55 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<h1>Home</h1>
+
+<a href="/demo">Demo</a>
+<a href="/auth">Auth</a>
+<a href="/app">App</a>
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,1 @@
+<h1>App Content</h1>
